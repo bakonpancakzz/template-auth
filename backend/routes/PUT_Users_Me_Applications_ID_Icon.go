61df45fcad7b9d81979b73bf2e9690c2f85cda3a
bakonpancakz <bakonpancakz@gmail.com>
2025-11-10 04:28:34 -0800

    Image Upload Endpoints
    finally

--- backend/routes/PUT_Users_Me_Applications_ID_Icon.go
+++ backend/routes/PUT_Users_Me_Applications_ID_Icon.go
@@ -1,5 +1,112 @@
 package routes
 
-import "net/http"
+import (
+	"errors"
+	"io"
+	"math"
+	"net/http"
+	"strconv"
 
-func PUT_Users_Me_Applications_ID_Icon(w http.ResponseWriter, r *http.Request) {}
+	"github.com/bakonpancakz/template-auth/tools"
+	"github.com/jackc/pgx/v5"
+)
+
+/*
+ * Technically we should check to see if the user actually owns the application
+ * before committing to all this effort, but the chances of this happening are low
+ * and the ratelimit is so aggressive that I feel it's worth skipping a preemptive
+ * database query :P
+ */
+
+func PUT_Users_Me_Applications_ID_Icon(w http.ResponseWriter, r *http.Request) {
+
+	session := tools.GetSession(r)
+	if session.ApplicationID != tools.SESSION_NO_APPLICATION_ID {
+		tools.SendClientError(w, r, tools.ERROR_OAUTH2_USERS_ONLY)
+		return
+	}
+	snowflake, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+	if err != nil {
+		tools.SendClientError(w, r, tools.ERROR_UNKNOWN_APPLICATION)
+		return
+	}
+	if err := r.ParseMultipartForm(math.MaxInt64); err != nil {
+		tools.SendClientError(w, r, tools.ERROR_BODY_INVALID_TYPE)
+		return
+	}
+
+	// Copy incoming image to memory
+	var uploadData []byte
+	var uploadHash string
+	var uploadOK bool
+	if file, _, err := r.FormFile("image"); err != nil {
+		tools.SendClientError(w, r, tools.ERROR_BODY_INVALID_FIELD)
+		return
+	} else if data, err := io.ReadAll(file); err != nil {
+		tools.SendClientError(w, r, tools.ERROR_BODY_INVALID_DATA)
+		return
+	} else {
+		file.Close()
+		uploadData = data
+	}
+
+	// Resize and store incoming image
+	options := tools.ImageOptionsIcons
+	if ok, hash := tools.ImageHandler(w, r, options, session.UserID, uploadData); !ok {
+		return
+	} else {
+		uploadHash = hash
+	}
+	defer func(id int64, hash string) {
+		if !uploadOK && hash != "" {
+			// Delete any possible leftover image files
+			paths := tools.ImagePaths(options, id, hash)
+			if err := tools.Storage.Delete(paths...); err != nil {
+				tools.LoggerStorage.Error("Failed to delete leftover icon", map[string]any{
+					"paths": paths,
+					"error": err.Error(),
+				})
+			}
+		}
+	}(session.UserID, uploadHash)
+
+	// Store updated image hash
+	ctx, cancel := tools.NewContext()
+	defer cancel()
+
+	var previousHash *string
+	err = tools.Database.QueryRow(ctx,
+		`UPDATE auth.applications SET
+			updated   = CURRENT_TIMESTAMP,
+			icon_hash = $1
+		WHERE id = $2 AND user_id = $3
+		RETURNING icon_hash`,
+		uploadHash,
+		snowflake,
+		session.UserID,
+	).Scan(&previousHash)
+	if errors.Is(err, pgx.ErrNoRows) {
+		tools.SendClientError(w, r, tools.ERROR_UNKNOWN_APPLICATION)
+		return
+	}
+	if err != nil {
+		tools.SendServerError(w, r, err)
+		return
+	}
+	uploadOK = true
+
+	// Delete previous images (if any)
+	go func(hash *string) {
+		if hash != nil {
+			paths := tools.ImagePaths(options, session.UserID, *hash)
+			if err := tools.Storage.Delete(paths...); err != nil {
+				tools.LoggerStorage.Error("Failed to delete previous icon", map[string]any{
+					"paths": paths,
+					"error": err.Error(),
+				})
+			}
+		}
+	}(previousHash)
+
+	w.WriteHeader(http.StatusNoContent)
+}
--- backend/routes/PUT_Users_Me_Banner.go
+++ backend/routes/PUT_Users_Me_Banner.go
@@ -1,5 +1,98 @@
 package routes
 
-import "net/http"
+import (
+	"errors"
+	"io"
+	"math"
+	"net/http"
 
-func PUT_Users_Me_Banner(w http.ResponseWriter, r *http.Request) {}
+	"github.com/bakonpancakz/template-auth/tools"
+	"github.com/jackc/pgx/v5"
+)
+
+func PUT_Users_Me_Banner(w http.ResponseWriter, r *http.Request) {
+
+	session := tools.GetSession(r)
+	if session.ApplicationID != tools.SESSION_NO_APPLICATION_ID {
+		tools.SendClientError(w, r, tools.ERROR_OAUTH2_USERS_ONLY)
+		return
+	}
+	if err := r.ParseMultipartForm(math.MaxInt64); err != nil {
+		tools.SendClientError(w, r, tools.ERROR_BODY_INVALID_TYPE)
+		return
+	}
+
+	// Copy incoming image to memory
+	var uploadData []byte
+	var uploadHash string
+	var uploadOK bool
+	if file, _, err := r.FormFile("image"); err != nil {
+		tools.SendClientError(w, r, tools.ERROR_BODY_INVALID_FIELD)
+		return
+	} else if data, err := io.ReadAll(file); err != nil {
+		tools.SendClientError(w, r, tools.ERROR_BODY_INVALID_DATA)
+		return
+	} else {
+		file.Close()
+		uploadData = data
+	}
+
+	// Resize and store incoming image
+	options := tools.ImageOptionsBanners
+	if ok, hash := tools.ImageHandler(w, r, options, session.UserID, uploadData); !ok {
+		return
+	} else {
+		uploadHash = hash
+	}
+	defer func(id int64, hash string) {
+		if !uploadOK && hash != "" {
+			// Delete any possible leftover image files
+			paths := tools.ImagePaths(options, id, hash)
+			if err := tools.Storage.Delete(paths...); err != nil {
+				tools.LoggerStorage.Error("Failed to delete leftover banner", map[string]any{
+					"paths": paths,
+					"error": err.Error(),
+				})
+			}
+		}
+	}(session.UserID, uploadHash)
+
+	// Store updated image hash
+	ctx, cancel := tools.NewContext()
+	defer cancel()
+
+	var previousHash *string
+	err := tools.Database.QueryRow(ctx,
+		`UPDATE auth.profiles SET
+			updated 	= CURRENT_TIMESTAMP,
+			banner_hash = $1
+		WHERE id = $2
+		RETURNING banner_hash`,
+		uploadHash,
+		session.UserID,
+	).Scan(&previousHash)
+	if errors.Is(err, pgx.ErrNoRows) {
+		tools.SendClientError(w, r, tools.ERROR_UNKNOWN_USER)
+		return
+	}
+	if err != nil {
+		tools.SendServerError(w, r, err)
+		return
+	}
+	uploadOK = true
+
+	// Delete previous images (if any)
+	go func(hash *string) {
+		if hash != nil {
+			paths := tools.ImagePaths(options, session.UserID, *hash)
+			if err := tools.Storage.Delete(paths...); err != nil {
+				tools.LoggerStorage.Error("Failed to delete previous banner", map[string]any{
+					"paths": paths,
+					"error": err.Error(),
+				})
+			}
+		}
+	}(previousHash)
+
+	w.WriteHeader(http.StatusNoContent)
+}
